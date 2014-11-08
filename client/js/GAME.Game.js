348a85a22232ae994aa31840c6d96f5df12f068d
Gattaka <gattakahynca@seznam.cz>
2014-11-08 12:15:10 +0100

    Player navigating a camera lock

--- client/js/GAME.Game.js
+++ client/js/GAME.Game.js
@@ -10,21 +10,19 @@ GAME.Game = function() {
     this.sceneOrtho = new THREE.Scene();
     this.cameraOrtho = undefined;
 
-    this.level = undefined;
     this.hud = undefined;
+    this.level = undefined;
+    this.player = undefined;
 
     this.keyboard = new THREEx.KeyboardState();
     this.clock = new THREE.Clock();
-    this.controls = undefined;
     this.stats = undefined;
 
     this.animator = new GAME.Animator();
 
-    this.player = undefined;
-
-    this.initControls = function() {
-	this.controls = new THREE.OrbitControls(this.camera, this.container);
-    };
+    this.eventsHelper = {
+	mouseDown : false
+    }
 
     this.createStats = function(container) {
 	var stats = new Stats();
@@ -35,9 +33,9 @@ GAME.Game = function() {
 	return stats;
     };
 
-    this.initScreenEvents = function() {
-	THREEx.WindowResize(this.renderer, this.camera);
-	THREEx.WindowResize(this.renderer, this.cameraOrtho);
+    this.initScreenEvents = function(game) {
+	THREEx.WindowResize(game.renderer, game.camera);
+	THREEx.WindowResize(game.renderer, game.cameraOrtho);
 	THREEx.FullScreen.bindKey({
 	    charCode : 'm'.charCodeAt(0)
 	});
@@ -60,7 +58,8 @@ GAME.Game = function() {
 	    game.renderer.render(game.sceneOrtho, game.cameraOrtho);
 	    THREE.AnimationHandler.update(delay);
 
-	    game.controls.update();
+	    // game.controls.update();
+
 	    game.stats.update();
 	    game.hitCheck(game);
 	    game.player.animate(delay);
@@ -80,70 +79,24 @@ GAME.Game = function() {
 	// this.level.plantObject(game.player.mesh);
     }
 
-    this.createPlayer = function(scene) {
-	var player = new GAME.Player(scene);
-	this.player = player;
-    }
-
-    this.mouseControls = function(game) {
-	var domEvents = new THREEx.DomEvents(game.camera, game.renderer.domElement)
-	var p = game.player;
-	domEvents.addEventListener(game.level.terrain.mesh, "click", function(event) {
-	    if (p.mesh == undefined)
-		return;
-	    var position = {
-		x : p.mesh.position.x,
-		z : p.mesh.position.z
-	    };
-	    var target = {
-		x : event.intersect.point.x,
-		z : event.intersect.point.z
-	    };
-
-	    var speed = 0.3; // jednotek za sekundu
-	    var a = target.z - position.z;
-	    var b = target.x - position.x;
-	    var distance = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-
-	    // protáhní stávající cíl jinak se nakonci bude postava koukat jiným směrem
-	    // (počátek se otočí kolem cílového bodu a pohled se "sveze" jiným směrem)
-	    // musím zachovat poměr jinak změním směr, takže jednu dám jako 100 a druhou
-	    // jako 100 * poměr jejich původních délek
-	    var lookTargetZ = target.z + (a > 0 ? 100 : -100);
-	    var lookTargetX = target.x + (b > 0 ? 100 : -100) * Math.abs(b/a);
-
-	    var line;
-	    var tween = new TWEEN.Tween(position).to(target, distance / speed);
-	    tween.onStart(function() {
-		p.state = GAME.Player.WALK;
-		var material = new THREE.LineBasicMaterial({
-		    color : 0x0000ff
-		});
-		var geometry = new THREE.Geometry();
-		geometry.vertices.push(new THREE.Vector3(position.x, 50, position.z));
-		geometry.vertices.push(new THREE.Vector3(target.x, 50, target.z));
-		line = new THREE.Line(geometry, material);
-		game.scene.add(line);
-	    });
-	    tween.onUpdate(function() {
-		// musí se přepisovat -- v případě, že je během chůze zadán nový cíl, může se stát, že jeho původní
-		// onComplete nastaví předčasně p.state = GAME.Player.STAND; ačkoliv už se pokračuje v novém pochodu
-		p.state = GAME.Player.WALK;
-		p.mesh.position.x = position.x;
-		p.mesh.position.z = position.z;
-		game.level.plantObject(p.mesh);
-		// počátek je u nohou, takže musí "koukat" na pozici přímo, kde stojí
-		p.mesh.lookAt(new THREE.Vector3(lookTargetX, p.mesh.position.y, lookTargetZ));
-	    });
-	    tween.onComplete(function() {
-		p.state = GAME.Player.STAND;
-		game.scene.remove(line);
-	    });
-	    tween.start();
-	}, false);
+    this.registerTerrainEvents = function(game) {
+	game.domEvents.addEventListener(game.level.terrain.mesh, "mousedown", function(event) {
+	    game.eventsHelper.mouseDown = true;
+	    game.player.navigatePlayer(game, event);
+	});
+	game.domEvents.addEventListener(game.level.terrain.mesh, "mouseup", function(event) {
+	    game.eventsHelper.mouseDown = false;
+	});
+	game.domEvents.addEventListener(game.level.terrain.mesh, "mousemove", function(event) {
+	    // TODO filtering
+	    game.player.navigatePlayer(game, event);
+	});
     }
 
     this.run = function() {
+	/*
+	 * RENDERER
+	 */
 	if (Detector.webgl) {
 	    this.renderer = new THREE.WebGLRenderer({
 		antialias : true
@@ -152,11 +105,9 @@ GAME.Game = function() {
 	    this.renderer = new THREE.CanvasRenderer();
 	}
 
-	// Set the background color of the renderer to black, with full opacity
 	this.renderer.setClearColor(0x000000, 1);
 	this.renderer.autoClear = false;
 	this.renderer.shadowMapEnabled = true;
-	// this.renderer.shadowMapType = THREE.PCFShadowMap;
 	this.renderer.shadowMapType = THREE.PCFSoftShadowMap;
 
 	var SCREEN_WIDTH = window.innerWidth;
@@ -165,29 +116,41 @@ GAME.Game = function() {
 	this.renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 	this.container.appendChild(this.renderer.domElement);
 
+	/*
+	 * CAMERA, SCENE
+	 */
 	var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 20000;
 	this.camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
 	this.scene.add(this.camera);
-	// this.camera.position.set(50, 40, -80);
-	this.camera.position.set(200, 300, 400);
-	this.camera.lookAt(this.scene.position);
+	this.scene.fog = new THREE.Fog(0x34583e, 0, 10000);
 
 	this.cameraOrtho = new THREE.OrthographicCamera(-SCREEN_WIDTH / 2, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
 		-SCREEN_HEIGHT / 2, 1, 10);
 	this.cameraOrtho.position.z = 10;
 
-	this.level = new GAME.Level(this.scene);
+	/*
+	 * EVENT ENGINE
+	 */
+	this.domEvents = new THREEx.DomEvents(this.camera, this.renderer.domElement);
+
+	/*
+	 * UTIL OBJEKTY
+	 */
 	this.hud = new GAME.HUD(this.sceneOrtho);
-
 	this.scene.add(new THREE.AxisHelper(100));
-	this.createPlayer(this.scene);
-	this.mouseControls(this);
-
-	this.initScreenEvents();
-	this.initControls();
 	this.stats = this.createStats(this.container);
 
-	this.scene.fog = new THREE.Fog(0x34583e, 0, 10000);
+	/*
+	 * LEVEL
+	 */
+	this.level = new GAME.Level(this.scene);
+	this.player = new GAME.Player(this);
+
+	/*
+	 * EVENTS -- již na připravených objektech
+	 */
+	this.registerTerrainEvents(this);
+	this.initScreenEvents(this);
 
 	this.animateScene();
     };
--- client/js/GAME.Player.js
+++ client/js/GAME.Player.js
@@ -1,6 +1,6 @@
 var GAME = GAME || {};
 
-GAME.Player = function(scene) {
+GAME.Player = function(game) {
     var player = this;
     var loader = new THREEx.UniversalLoader()
     loader.load('../models/armature_test.dae', function(object3d) {
@@ -12,13 +12,16 @@ GAME.Player = function(scene) {
 	    }
 	});
 
-	object3d.scale.set(5, 5, 5);
+	var scale = 1;
+	object3d.scale.set(scale, scale, scale);
 	object3d.castShadow = true;
 	object3d.receiveShadow = true;
 	player.mesh = object3d;
 	var box = new THREE.Box3().setFromObject(object3d);
 	player.height = Math.abs(box.max.y - box.min.y);
-	scene.add(object3d);
+	game.scene.add(object3d);
+
+	player.targetCamera(game.camera, player);
     });
 
 }
@@ -61,9 +64,72 @@ GAME.Player.prototype = {
 	    }
 	}
 
-	// console.log("currentTime: " + currentTime);
-	// console.log("currentKeyFrame: " + currentKeyFrame);
-
 	this.oldState = this.state;
     },
+
+    targetCamera : function(camera, player) {
+	camera.position.x = player.mesh.position.x + 50;
+	camera.position.y = player.mesh.position.y + 100;
+	camera.position.z = player.mesh.position.z + 100;
+	camera.lookAt(player.mesh.position);
+    },
+
+    navigatePlayer : function(game, event) {
+	var p = game.player;
+	if (p.mesh == undefined || game.eventsHelper.mouseDown == false)
+	    return;
+	var position = {
+	    x : p.mesh.position.x,
+	    z : p.mesh.position.z
+	};
+	var target = {
+	    x : event.intersect.point.x,
+	    z : event.intersect.point.z
+	};
+
+	var speed = 0.1; // jednotek za sekundu
+	var a = target.z - position.z;
+	var b = target.x - position.x;
+	var distance = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+	// protáhní stávající cíl jinak se nakonci bude postava koukat jiným směrem
+	// (počátek se otočí kolem cílového bodu a pohled se "sveze" jiným směrem)
+	// musím zachovat poměr jinak změním směr, takže jednu dám jako 100 a druhou
+	// jako 100 * poměr jejich původních délek
+	var lookTargetZ = target.z + (a > 0 ? 100 : -100);
+	var lookTargetX = target.x + (b > 0 ? 100 : -100) * Math.abs(b / a);
+
+	var line;
+	var tween = new TWEEN.Tween(position).to(target, distance / speed);
+	tween.onStart(function() {
+	    p.state = GAME.Player.WALK;
+	    var material = new THREE.LineBasicMaterial({
+		color : 0x0000ff
+	    });
+	    var geometry = new THREE.Geometry();
+	    geometry.vertices.push(new THREE.Vector3(position.x, 50, position.z));
+	    geometry.vertices.push(new THREE.Vector3(target.x, 50, target.z));
+	    line = new THREE.Line(geometry, material);
+	    game.scene.add(line);
+	});
+	tween.onUpdate(function() {
+	    // musí se přepisovat -- v případě, že je během chůze zadán nový cíl, může se stát, že jeho původní
+	    // onComplete nastaví předčasně p.state = GAME.Player.STAND; ačkoliv už se pokračuje v novém pochodu
+	    p.state = GAME.Player.WALK;
+	    p.mesh.position.x = position.x;
+	    p.mesh.position.z = position.z;
+	    game.level.plantObject(p.mesh);
+	    // počátek je u nohou, takže musí "koukat" na pozici přímo, kde stojí
+	    p.mesh.lookAt(new THREE.Vector3(lookTargetX, p.mesh.position.y, lookTargetZ));
+
+	    // CAMERA
+	    p.targetCamera(game.camera, p);
+
+	});
+	tween.onComplete(function() {
+	    p.state = GAME.Player.STAND;
+	    game.scene.remove(line);
+	});
+	tween.start();
+    }
 }
